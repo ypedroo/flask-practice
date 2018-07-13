a54513d1540b7f3fd7a619c5bb57423349bb8907
pedro <pedromota99@gmail.com>
2018-07-13 08:45:16 -0300

    adding bootstrap

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -32,7 +32,7 @@ def criar():
     console = request. form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return render_template('lista.html', titulo='Jogo', jogos=lista)
+    return redirect('/')
 
 
 app.run(debug=True)
